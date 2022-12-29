ae92215a7e1eeeb02d7d0529a0ba8eb5f97eddc5
Sheila Afros <safros@uwaterloo.ca>
2022-12-29 18:02:59 -0500

    In Progress try to add db
    csv create db in progress

--- main.py
+++ main.py
@@ -13,6 +13,9 @@ db = SQLAlchemy(app)
 class DataForModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     cost = db.Column(db.String(80), unique=True)
+    distance = db.Column(db.String(80), unique=True)
+    truck = db.Column(db.String(80), unique=True)
+    demand = db.Column(db.Integer(), unique=True)
 
     def __repr__(self):
         return "<Cost: {}>".format(self.cost)
