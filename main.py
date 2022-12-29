9cb2adb7fc46e6dc46f121fda08b9139972dcf82
Sheila Afros <safros@uwaterloo.ca>
2022-12-29 18:05:03 -0500

    fix
    fixed with app.app_context()

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
+app.app_context().push()
 
 class DataForModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
