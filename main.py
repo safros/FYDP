dfee21a1fa4b9c022dc095db773bbf0315ad11c2
Sheila Afros <safros@uwaterloo.ca>
2023-02-21 22:57:58 -0500

    shortest path building graphs

--- main.py
+++ main.py
@@ -451,9 +451,9 @@ def dijstra ():
 
     for row in dataDistances:
         # fetch the speed corresponding to the correct origin destination
-        speedNeeded =db.engine.execute("SELECT Speed_Limit FROM speedLimit WHERE Origin_ID LIKE '{}' AND Destination_ID LIKE '{}'".format(str(row[0]), str(row[1]))).fetchall()
+        speedNeeded =db.engine.execute("SELECT Speed_Limit FROM speedLimit WHERE Origin_ID LIKE '{}' AND Destination_ID LIKE '{}'".format(str(row[1]), str(row[2]))).fetchall()
         #take that speed and get the correct emissions for exactly one type of truck
-        dataEmissions = db.engine.execute("SELECT costperKm FROM emissions WHERE typeTruck LIKE 'Single Unit Short Haul' AND speed LIKE '{}' ".format(speedNeeded)).fetchall()
+        dataEmissions = db.engine.execute("SELECT costperKm FROM emissions WHERE typeTruck LIKE 'Single Unit Short Haul' AND speed LIKE '{}' ".format(str(speedNeeded[0]))).fetchall()
         init_graph_Emissions[str(row[1])][str(row[2])] = str(row[3])*dataEmissions
 
     graphDamage = capstone.Graph(nodes, init_graph_Damages)
